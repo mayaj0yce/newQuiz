13239a308159a2aad3d7e318212371f0e1bcc4c7
mayaj0yce <m.j.thayne@gmail.com>
2023-09-05 00:39:28 -0600

    workingon one at a time

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('test');
+
+var h1El = document.querySelector('#h1');
+var h2El = document.querySelector('#h2');
+var h3El = document.querySelector('#h3');
+var pEl = document.querySelector('#p');
