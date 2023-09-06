07a5a987026a7e49ec7089047c9e5460133d55e4
mayaj0yce <m.j.thayne@gmail.com>
2023-09-06 17:40:49 -0600

    save point

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ var h1El = document.querySelector('#h1');
 var h2El = document.querySelector('#h2');
 var h3El = document.querySelector('#h3');
 var pEl = document.querySelector('#p');
+
+var time = document.querySelector(".time");
+
+// C:\Users\mjtha\downloads\sneks\04-Web-APIs\01-Activities\09-Ins_Timers-Intervals\script.js
