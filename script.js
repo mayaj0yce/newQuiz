f011be04c982825faafbbca9c981d85e8f3a2b53
mayaj0yce <m.j.thayne@gmail.com>
2023-10-05 00:38:16 -0600

    usequestions as variables and or functions

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var timeEl = document.querySelector('time');
 var start = document.querySelector('start');
 var main = document.getElementById('main');
+let message = document.querySelector("#questions");
+var oneDiv = document.getElementById('questionOne')
+
 
 var startButton = document.querySelector(".start-button")
 
 function startQuiz() {
     showQuestions()
 }
+
 
 window.onload = function () {
   document.getElementById('questions').style.display = 'none';
@@ -19,8 +23,50 @@ function showQuestions() {
   } else {
     x.style.display = "none";
   }
-
 }
+
+function startFunction() {
+  message.innerHTML = question1 ;
+}
+
+var question1 = 'Where is the console? <button>A<?button><button onClick="clickMe()" > here </button>'
+//this is the path from one question to the next. if i can get the buttons put in then I can get it sorted
+
+function clickMe() {
+  message.innerHTML = 'this'
+}
+//this shows 'this' when the here button has been pressed
+
+
+// function questions() {
+// {
+//   question: 
+//   ["where is the console?"]
+
+//   answers: 
+//   a: vsCode
+//   b: html document
+//   c: inspect 
+//   d: settings
+//   
+//   correctAnswer = c
+// },
+// }
+
+// if startWQuiz() then show showQuestions(),
+// show question one by one
+// showOne() { var x = questionOne } 
+// rightAnswer() { switch to next page }
+// else (or do a new function for wrongAnswer) flash red and sub 1 point from user score
+// onLoad userscore = 5. on end user score can be saved with initials to the localStorage
+
+// if question answer === right then move to next question
+// else sub 5 sec and alert 'wrong' 
+
+// question must be selected as correct to move onto the next question
+// format question field and use button choices as correct / not
+
+
 
 function endGame() {
     console.log('done')
