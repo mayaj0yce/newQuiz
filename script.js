602a3bdcf2e32e302b57f5dfe129263d609e85d2
mayaj0yce <m.j.thayne@gmail.com>
2023-10-07 01:34:15 -0600

    using innerHTML i was able to present all questions

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ var timeEl = document.querySelector('time');
 var start = document.querySelector('start');
 var main = document.getElementById('main');
 let message = document.querySelector("#questions");
-var oneDiv = document.getElementById('questionOne')
+var oneDiv = document.getElementById('questionOne');
+var userPoints = 60;
 
-
-var startButton = document.querySelector(".start-button")
+var startButton = document.querySelector(".start-button");
 
 function startQuiz() {
     showQuestions()
-}
-
-
-// window.onload = function () {
- 
-// }
+    hideButton()
+};
 
 function hideButton() {
   var x = document.getElementById("mad");
@@ -24,14 +20,16 @@ function hideButton() {
   } else {
     x.style.display = "block";
   }
-}
+};
 
-
+//hides the questions and shows the start button
 window.onload = function () {
   document.getElementById('questions').style.display = 'none';
   document.getElementById('mad').style.display = 'block';
+  document.getElementById('results').style.display = 'none';
 }
 
+//on click the start button will be hidden and the questions will show
 function showQuestions() {
    var x = document.getElementById("questions");
    if (x.style.display === "none") {
@@ -42,64 +40,66 @@ function showQuestions() {
 }
 
 function startFunction() {
-  message.innerHTML = question1 ;
+  message.innerHTML = question1;
 }
 
 //Quesrion one
-var question1 = 'Where is the console? <button>A<?button><button onClick="clickMe()" > here </button>'
-//this is the path from one question to the next. if i can get the buttons put in then I can get it sorted
+var question1 = 'Where is the console? <button onClick="wrongGuess()">A:vscode<?button><button onClick="wrongGuess()">B: the server <?button><button onClick="clickMe()">C: under "inspect" </button><button onClick="wrongGuess()">D: in the terminal<?button>'
+
+function wrongGuess() {
+  console.log('oopsie -5 ')
+  // alert(message, 'try again')
+  // userScore -5 
+}
+
 
 // Question two
 function clickMe() {
   // add point to user score
-  message.innerHTML = ['this', '<button onClick="clickMe2()">A<?button>']
+  message.innerHTML = ['A page can be styled through all of the following EXCEPT: <button onClick="wrongGuess()">A:html<?button><button onClick="wrongGuess()">B: CSS<?button><button onClick="wrongGuess()">C: javascript </button><button onClick="clickMe2()">D: .env <?button>']
 }
 
 //Question three
 function clickMe2() {
   // add point to user score
-  message.innerHTML = ['hellooooo', '<button onClick="clickMe3()">A<?button>']
+  message.innerHTML = [' Pick the right format for an "if else statement" <button onClick="clickMe3()"> A: if () {} else {} <?button><button onClick="wrongGuess()"> B: if {} else ()<?button><button onClick="wrongGuess()"> C: if.else.{} </button><button onClick="wrongGuess()"> D: <if>  </else> <?button>']
 }
 
 //Question four
 function clickMe3() {
   // add point to user score
-  message.innerHTML = ['this', '<button onClick="clickMe4()"> A <?button> ']
+  message.innerHTML = [' Which of these represents the first object of an array: <button onClick="wrongGuess()">A: -1<?button><button onClick="clickMe4()">B: 0<?button><button onClick="wrongGuess()"> C: 1 <?button> <button onClick="wrongGuess()">D: 0.0 <?button?>']
 }
 
 //Question five
 function clickMe4() {
   // add point to user score
-  message.innerHTML = ['this', '<button onClick="results()"> A <?button>']
+  message.innerHTML = ['By using async what can you make a function do: <button onClick="results()"> A: wait <?button> <button onClick="wrongGuess()"> B: stutter step <?button> <button onClick="wrongGuess()"> C: trigger <?button><button onClick="wrongGuess()"> D: coil <?button>']
 }
 
 function results() {
-  message.innerHTML = 'user results'
+  let userScore = 8
+  message.innerHTML = ['Your Score:',  userScore];
+  showResults()
 };
-//this shows 'this' when the here button has been pressed
 
 
-// function questions() {
-// {
-//   question: 
-//   ["where is the console?"]
 
-//   answers: 
-//   a: vsCode
-//   b: html document
-//   c: inspect 
-//   d: settings
-//   
-//   correctAnswer = c
-// },
-// }
+function showResults() {
+  var x = document.getElementById("results");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
 
 // if startWQuiz() then show showQuestions(),
 // show question one by one
 // showOne() { var x = questionOne } 
 // rightAnswer() { switch to next page }
 // else (or do a new function for wrongAnswer) flash red and sub 1 point from user score
-// onLoad userscore = 5. on end user score can be saved with initials to the localStorage
+// onLoad userscore = 60. on end user score can be saved with initials to the localStorage
 
 // if question answer === right then move to next question
 // else sub 5 sec and alert 'wrong' 
