48c4e5fb004ff95ea4f678f571ab588e770da475
mayaj0yce <m.j.thayne@gmail.com>
2023-10-11 00:22:52 -0600

    done, scores save, need reload but will show

--- score.js
+++ score.js
@@ -1,13 +1,42 @@
-const userInitials = document.getElementById('userInitials');
-const saveScoreBtn = document.getElementById('saveButton');
+const userInitials = document.getElementById("userInitials");
+const saveScoreBtn = document.getElementById("saveScore");
+const lastScore = localStorage.getItem("lastScore");
+const finalScore = document.getElementById("finalScore");
 
-const lastScore = localStorage.getItem('lastScore');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const maxHighScores = 5;
 finalScore.innerText = lastScore;
 
 userInitials.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !userInitials.ariaValueMax;
+    saveScoreBtn.disabled = !userInitials.value;
 });
 
-saveHighScore = e => {
+saveHighScore = (e) => {
     e.preventDefault();
+
+    const score = {
+        score: lastScore,
+        name: userInitials.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
 };
+const highScoresContainer = document.getElementById("highScores");
+const highScoresList = document.getElementById("highScoresList");
+
+function displayHighScores() {
+    highScoresList.innerHTML = ""; // Clear the existing content
+
+    highScores.forEach((score, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${index + 1}. ${score.name}: ${score.score}`;
+        highScoresList.appendChild(listItem);
+    });
+}
+
+// Call the function to display high scores initially
+displayHighScores();
